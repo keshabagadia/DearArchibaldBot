d50ab6209c86907a94e9d7a6067a1591fb688af2
keshabagadia <knbagadi@andrew.cmu.edu>
2025-05-04 17:48:45 -0400

    refactored local data files (prompt and visitor)

--- src/data/visitors.js
+++ src/data/visitors.js
@@ -9,35 +9,38 @@
  * - badOutcome (string): "If the interaction goes poorly..."
  */
 
+function createVisitor(visitorData) {
+  return {
+    memory: 0,
+    goodOutcome: "Placeholder for a positive resolution.",
+    badOutcome: "Placeholder for a negative outcome.",
+    ...visitorData,
+  };
+}
+
 module.exports = [
-  {
+  createVisitor({
     id: 1,
     title: `A Worn-Out Visitor`,
-    description:
-      `Boris, a Badger, stumbles into your gathering place, visibly fatigued and carrying a heavy load.`,
+    description: `Boris, a Badger, stumbles into your gathering place, visibly fatigued and carrying a heavy load.`,
     level: 1,
     optionalScenarios: [
       `If this is his first time here, Boris explains that he has been working nonstop to gather enough food for the upcoming winter and feels overwhelmed. Write to find out how this discussion plays out.`,
       `Boris shows a cracked, worn-out wagon wheel that he’s been using to transport goods.`,
       `Boris asks for a place to rest for a while but feels guilty about taking a break. How do your characters try to reassure him?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 2,
     title: `A Small Visitor`,
-    description:
-      `A field mouse comes in, his small child in tow. It is extremely clear that the kid is in serious need of some manners.`,
+    description: `A field mouse comes in, his small child in tow. It is extremely clear that the kid is in serious need of some manners.`,
     level: 1,
     optionalScenarios: [
       `The kid immediately breaks something in the room. What is it?`,
       `The field mouse sighs and says he hasn't slept in days because nobody is around to watch the kid right now except him. He’s getting pressure from his village, who don’t understand why he can’t “pull his own weight.” He asks for help.`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 3,
     title: `A Troubled Visitor`,
     description: `A mushroom sprite named Blink comes in, looking nervous.`,
@@ -47,10 +50,8 @@ module.exports = [
       `The sprite obviously has a fae curse and is trying to hide it. Describe the curse and whether you point it out, and what happens next.`,
       `Think about your character and a goal they have. The sprite confesses that they have, somehow, accidentally or on purpose, gotten your character further away from it. What is it, and what happens next?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 4,
     title: `A Competitive Visitor`,
     description: `An old squirrel comes in, looking thoughtful.`,
@@ -59,14 +60,11 @@ module.exports = [
       `The squirrel challenges one of you to a game of skill. What does he wager, and what do you wager in return? What game? Roll to see who wins. What happens next?`,
       `The squirrel is feeling wistful. He tells you a story about a time he felt judged for resting. Write to find out the short conversation between him and your character. Roll to see if the conversation goes well.`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 5,
     title: `A Bazaar Visitor`,
-    description:
-      `A traveling bazaar rolls up to the gathering place and sets up for the day.`,
+    description: `A traveling bazaar rolls up to the gathering place and sets up for the day.`,
     level: 1,
     optionalScenarios: [
       `One of their members looks lost and upset. Write to find out the conversation you have with them.`,
@@ -75,10 +73,8 @@ module.exports = [
       `There is an exotic fruit and nuts stall. Maybe something is worth sampling?`,
       `One of their members is bragging that they're the fastest creature in the wood. What happens next?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 6,
     title: `An Exiled Visitor`,
     description: `Delilah, a Mole, walks into your gathering place.`,
@@ -88,10 +84,8 @@ module.exports = [
       `Delilah has been working on an invention, a small golden box that she says allows people to go back in time five minutes. It's not currently working. She says it often gains other capabilities or properties as well. What happens next?`,
       `Delilah is looking for a temporary place to stay (unless she is already staying at the gathering place). But she is reluctant to stay here for fear of imposing. What happens next?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 7,
     title: `A Hungry Visitor`,
     description: `Rufus the Frog strides in.`,
@@ -101,35 +95,28 @@ module.exports = [
       `Rufus wants to host a grand feast but has only a single turnip to start with.`,
       `Rufus examines your gathering place and describes how he could improve it.`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 8,
     title: `A Mischievous Visitor`,
-    description:
-      `Pip, a Chipmunk, arrives at your gathering place, grinning mischievously.`,
+    description: `Pip, a Chipmunk, arrives at your gathering place, grinning mischievously.`,
     level: 2,
     optionalScenarios: [
       `Pip has switched everyone’s belongings around as a prank and now can’t remember where anything goes. He is holding items from all around this area.`,
       `Pip insists he can perform magic tricks and wants to show off his skills. What happens next?`,
       `Pip challenges your characters to a race but has set up various, increasingly dangerous obstacles along the way. What happens next?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
-  {
+  }),
+  createVisitor({
     id: 9,
     title: `A Feral Visitor`,
-    description:
-      `Clay, a Raccoon, shuffles in. He is well-known for living on the fringes of polite animal society.`,
+    description: `Clay, a Raccoon, shuffles in. He is well-known for living on the fringes of polite animal society.`,
     level: 2,
+    memory: 1, // this one overrides the default
     optionalScenarios: [
       `Clay challenges a long-held belief your character has related to their Flaw. What is it? Does he persuade them to see things a different way?`,
       `Clay gets into an argument with another visitor. What is it about? What happens next?`,
       `Clay is good at reading others. He realizes you're hiding something. What is it? What happens next?`,
     ],
-    goodOutcome: `Placeholder for a positive resolution.`,
-    badOutcome: `Placeholder for a negative outcome.`,
-  },
+  }),
 ];
